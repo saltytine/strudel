946ecf28d3cd43f2922fe6d399a0b6788c803b85
Saltine <156854448+saltytine@users.noreply.github.com>
2025-08-15 12:30:27 -0400

    Update bass.js

--- bass.js
+++ bass.js
@@ -1,7 +1,16 @@
-samples('github:yaxu/clean-breaks')
 stack(
   note(`
-    [d2][c2][d2][g2][e2 e3][d2][f#2][b2]
-  `).s("gm_slap_bass_2").gain(2),
-  s("amen/4").fit().chop(32)
+    [d2]<[c2*2][c2]>[d2][g2][e2 e3][d2][f#2][b2]
+  `).s("gm_slap_bass_2").gain(4),
+  note(`
+    [{g b d}][{b d f#}]
+  `).s("gm_electric_guitar_jazz").gain(1),
+  note(`
+    <[{c, e, g}][{d, f#, a, c}]><[{e, g, b}][{g, b, d}]>
+  `).s("gm_epiano1"),
+  sound(`
+    [~ ~ ~ ~][sd ~ ~ ~][~ ~ ~ ~]<[sd ~ ~ ~] [cr ~ sd ~]>,
+    [hh*4][hh*6][hh*4][hh*4],
+    [bd ~*3][~*4][bd ~*3]<[~*4][{bd, oh} ~*3]>
+  `),
 )
